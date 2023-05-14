db9f6aa48e5078d5ad82d7081261576df641afe4
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2023-05-14 18:40:49 +0200

    ApplicationConfiguration

---
+++ src/main/java/org/example/springwebxmlexample/configuration/ApplicationConfiguration.java
@@ -0,0 +1,20 @@
+package org.example.springwebxmlexample.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("org.example")
+@EnableWebMvc
+public class ApplicationConfiguration {
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
